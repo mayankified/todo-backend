be34d49dedbcf5dd580c1de603330ff7bf39c2ea
MAYANK <mayanklander.com>
2024-01-22 12:03:38 +0530

    changed domain

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],// Replace with your frontend domain
+  origin: [process.env.LINK, 'https://tinudo.vercel.app'],// Replace with your frontend domain
   credentials: true,
 }));
 
